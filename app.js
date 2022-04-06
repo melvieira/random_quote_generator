631986a5285a577674009302faf6b799076b737e
melvieira <melanie02125@gmail.com>
2022-04-05 22:55:01 -0400

    final edits

--- app.js
+++ app.js
@@ -1,35 +1,27 @@
 
 let quote = [
-    "Genius is one percent inspiration and ninety-nine percent perspiration.",
-    "A house divided against itself cannot stand by Abraham Lincoln"
-    ,
-    "Difficulties increase the nearer we get to the goal by Johann Wolfgang von Goethe"
-    ,
-    "Fate is in your hands and no one elses by Byron Pulsifer",
-    "Nothing happens unless first we dream.",
-    "Life is a learning experience, only if you learn.",
-    "Self-complacency is fatal to progress.",
-    "What you give is what you get.",
-    "We can only learn to love by loving.",
-    "Trouble is only opportunity in work clothes.",
-    "Work out your own salvation. Do not depend on others.",
+        '"Genius is one percent inspiration and ninety-nine percent perspiration." -author Thomas Edison',
+      '   "You can observe a lot just by watching." -author Yogi Berra',
+        '"A house divided against itself cannot stand." -author Abraham Lincoln',
+          ' "Difficulties increase the nearer we get to the goal." -author Johann Wolfgang von Goethe',
+           '"Fate is in your hands and no one elses." -author Byron Pulsifer',
+        ' "Be the chief but never the lord." -author Lao Tzu',
+        '"Nothing happens unless first we dream." -author Carl Sandburg',
+        '"Well begun is half done." -author Aristotle',
+      '"Life is a learning experience, only if you learn." -Author Yogi Berra',
 ];
-
-// let quote = fetch('https://type.fit/api/quotes')
-// .then(response => response.json())
-// .then(data => console.log(data));
 
 
 function getNewQuote() {
     let todayQuote= quote[Math.floor(Math.random() * quote.length)];
    console.log(todayQuote);
-   document.getElementById("start").innerHTML = todayQuote;
+   document.getElementById("newQuote").innerHTML = todayQuote;
 };
 
 
 let genBtn = document.getElementById("btnGen");
 let buttonHandler = () => {
-    const userQuote = document.getElementById("start");
+    const userQuote = document.getElementById("newQuote");
     userQuote.value = getNewQuote();
 };
 // Event listener for generate quote button
